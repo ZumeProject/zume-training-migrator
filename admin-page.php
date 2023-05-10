5df5a2d8a2b861020b9d37dab971fe40bbe90b36
Chris Chasm <chris@chasm.solutions>
2023-05-10 12:42:56 -0600

    Update dt reports

--- admin-page.php
+++ admin-page.php
@@ -31,8 +31,8 @@ class Zume_Training_Migrator {
     /**
      * #1 SET VARIABLES
      */
-    public $loop_size = 500;
-    public $description = 'Template';
+    public $loop_size = 100;
+    public $description = 'Update sessions reports';
     public $title = 'Zume Training Migrator';
 
     /**
@@ -41,7 +41,7 @@ class Zume_Training_Migrator {
     public function source_query() {
         global $wpdb;
         return $wpdb->get_results(
-            "SELECT * FROM {$wpdb->prefix}posts WHERE post_type = 'zume_page' AND post_status = 'publish' ORDER BY ID ASC;"
+            "SELECT user_id, meta_value FROM wp_usermeta WHERE meta_key LIKE 'zume_group_%';"
             , ARRAY_A );
     }
 
@@ -49,14 +49,132 @@ class Zume_Training_Migrator {
      * #3 EDIT TASK TO EXECUTE
      */
     public function run_task( $result ) {
-
-        /**
-         * PROCESS TASK
-         */
-        dt_write_log( maybe_serialize( $result) );
+        $session = maybe_unserialize( $result['meta_value'] );
+        $users = [];
+        $users[] = $result['user_id'];
+        if ( isset( $session['coleaders'] ) && ! empty( $session['coleaders'] ) ) {
+            foreach( $session['coleaders'] as $coleader_email ) {
+                $u = get_user_by( 'email', $coleader_email );
+                if ( $u ) {
+                    $users[] = $u->ID;
+                }
+            }
+        }
+        foreach( $users as $user_id ) {
+            $list = $this->list();
+            foreach( $list as $item ) {
+                if ( !empty( $session[$item['key']] ) ) {
+                    $report = $this->get_array( $session, $item['number'], $item['key'], $user_id );
+                    $report['hash'] = hash( 'sha256', maybe_serialize( $report ) );
+                    $report['timestamp'] = time();
+                    $duplicate_found = $this->check_dup($report['hash']);
+                    if ( ! $duplicate_found ) {
+                        $this->insert( $report );
+                    }
+                }
+            }
+        }
 
     }
-
+    public function check_dup( $hash ) {
+        global $wpdb;
+        return $wpdb->get_row(
+            $wpdb->prepare(
+                "SELECT
+                    `id`
+                FROM
+                    wp_dt_reports
+                WHERE hash = %s AND hash IS NOT NULL;",
+                $hash
+            )
+        );
+    }
+    public function list() {
+        return [
+            [
+                'number' => '1',
+                'key' => 'session_1_complete',
+            ],
+            [
+                'number' => '2',
+                'key' => 'session_2_complete',
+            ],
+            [
+                'number' => '3',
+                'key' => 'session_3_complete',
+            ],
+            [
+                'number' => '4',
+                'key' => 'session_4_complete',
+            ],
+            [
+                'number' => '5',
+                'key' => 'session_5_complete',
+            ],
+            [
+                'number' => '6',
+                'key' => 'session_6_complete',
+            ],
+            [
+                'number' => '7',
+                'key' => 'session_7_complete',
+            ],
+            [
+                'number' => '8',
+                'key' => 'session_8_complete',
+            ],
+            [
+                'number' => '9',
+                'key' => 'session_9_complete',
+            ],
+            [
+                'number' => '10',
+                'key' => 'session_10_complete',
+            ],
+        ];
+    }
+    public function get_array( $session, $number, $session_label, $user_id ) {
+        return [
+            'user_id' => $user_id,
+            'parent_id' => null,
+            'post_id' => null,
+            'post_type' => null,
+            'type' => 'zume_session',
+            'subtype' => $number,
+            'payload' => null,
+            'value' => 0,
+            'lng' => $session['location_grid_meta']['lng'] ?? null,
+            'lat' => $session['location_grid_meta']['lat'] ?? null,
+            'level' => $session['location_grid_meta']['level'] ?? null,
+            'label' => $session['location_grid_meta']['label'] ?? null,
+            'grid_id' => $session['location_grid_meta']['grid_id'] ?? null,
+            'time_begin' => null,
+            'time_end' => strtotime( $session[$session_label] )
+        ];
+    }
+    public function insert( $report ) {
+        global $wpdb;
+        return $wpdb->insert( 'wp_dt_reports', $report,
+            [
+                '%d', // user_id
+                '%d', // parent_id
+                '%d', // post_id
+                '%s', // post_type
+                '%s', // type
+                '%s', // subtype
+                '%s', // payload
+                '%d', // value
+                '%f', // lng
+                '%f', // lat
+                '%s', // level
+                '%s', // label
+                '%d', // grid_id
+                '%d', // time_begin
+                '%d', // time_end
+                '%s', // timestamp
+                '%s', // hash
+            ] );
+    }
 
 
     
